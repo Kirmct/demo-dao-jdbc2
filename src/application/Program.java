b7f3b7fae6ac92c180e1216f713a96e3eca4bdf2
Kirmct de Abreu <kirmctneto@gmail.com>
2023-07-08 10:58:20 -0400

    seller entitie class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
         Department dp = new Department(1, "Books");
-        System.out.println(dp);
+        Seller seller = new Seller(21, "Kirmct", "kirmct@gmail.com", new Date(), 2000.00, dp);
+        System.out.println(seller);
     }
 }
